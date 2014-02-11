82a75a5bf656effae5bdb3e7b58e3180c93ed40d
Christopher <flare561@gmail.com>
2014-02-10 22:39:47 -0600

    Added individual irc bots per user

--- index.js
+++ index.js
@@ -13,59 +13,86 @@ module.exports = function(details) {
   var emoteFormatGame = details.emoteFormatGame || details.emoteFormat || '\u000303%s %s'; 
   
   var queue = [];
-  
+
+  var botsJoined = false;
+
   function sendMessage(msg) {
     if (steam.loggedOn) {
-      steam.sendMessage(details.chatroom, msg);
+      regex="[\\x02\\x1F\\x0F\\x16]|\\x03(\\d\\d?(,\\d\\d?)?)?";
+      message = msg.replace(new RegExp(regex, 'g'), "");
+      steam.sendMessage(details.chatroom, message);
     } else {
       queue.push(msg);
     }
   }
+
+  function createValidNick(originalNick) {
+    regex="[^a-zA-Z_\\-\\\\[\\]{}\\^`|][^a-zA-Z0-9_\\-\\\\[\\]{}\\^`|]?";
+    newNick = originalNick.replace(/ /g, "_").replace(new RegExp(regex, 'g'), "");
+    if (newNick.length > 0)
+      return newNick.slice(0,17);
+    else
+      return "User";
+  }
   
+  var steamIRCBots = {};
+
+
+  //Technically, this still isn't threadsafe in any way, but it seems to work better than any other method.
+  function joinSteamBots()
+  {
+    if (!botsJoined) {
+      if (steam.chatRooms[details.chatroom] != undefined)
+      {
+        console.log("joining bots")
+        for (user in steam.chatRooms[details.chatroom]) {
+          if(user != '76561198093596874')
+            joinUser(user);
+        }
+        botsJoined = true;
+      }
+      else
+      {
+        console.log("Not connected to chat, waiting 100ms")
+        setTimeout(joinSteamBots, 100);
+      }
+    }
+  }
+
+  function joinUser(user) {
+    steamIRCBots[user] = new (require('irc')).Client(details.server, createValidNick(steam.users[user].playerName) + "_s", {
+                                      channels: [details.channel], userName: "SteamBot", realName: steam.users[user].playerName
+                                    });
+    steamIRCBots[user].on('error', function(err) {
+        console.log('IRC error: ', err);
+      });
+  
+  }
+
+  function sendIRCMessage(user, msg) {
+    if (Object.keys(steamIRCBots).indexOf(user) != -1) {
+      steamIRCBots[user].say(details.channel, msg);
+    }
+    else {
+      irc.say(details.channel, '<' + steam.users[user].playerName + '> ' + msg);
+    }
+  }
+
   var irc = new (require('irc')).Client(details.server, details.nick, {
-    channels: [details.channel]
+    channels: [details.channel], userName: "SteamBot"
   });
   
-<<<<<<< HEAD
-=======
   irc.on('error', function(err) {
     console.log('IRC error: ', err);
   });
   
   irc.on('message' + details.channel, function(from, message) {
-    sendMessage('<' + from + '> ' + message);
-    
-    if (!steam.loggedOn)
-      return;
-    
-    var parts = message.match(/(\S+)\s+(.*\S)/);
-    
-    var triggers = {
-      '.k': 'kick',
-      '.kb': 'ban',
-      '.unban': 'unban'
-    };
-    
-    if (parts && parts[1] in triggers) {
-      irc.whois(from, function(info) {
-        if (info.channels.indexOf('@' + details.channel) == -1)
-          return; // not OP, go away
-        
-        Object.keys(steam.users).filter(function(steamID) {
-          return steam.users[steamID].playerName == parts[2];
-        }).forEach(function(steamID) {
-          steam[triggers[parts[1]]](details.chatroom, steamID);
-        });
-      });
-    } else if (message.trim() == '.userlist') {
-      Object.keys(steam.chatRooms[details.chatroom]).forEach(function(steamID) {
-        irc.notice(from, steam.users[steamID].playerName + ' http://steamcommunity.com/profiles/' + steamID);
-      });
-    }
+    if (from.slice(-2) != '_s')
+      sendMessage('<' + from + '> ' + message);
   });
   
   irc.on('action', function(from, to, message) {
-    if (to == details.channel) {
+    if (to == details.channel && from.slice(-2) != '_s') {
       sendMessage(from + ' ' + message);
     }
   });
@@ -87,18 +114,25 @@ module.exports = function(details) {
   });
   
   irc.on('join' + details.channel, function(nick) {
-    sendMessage(nick + ' has joined ' + details.channel);
+    if (nick.slice(-2) != '_s')
+      sendMessage(nick + ' has joined ' + details.channel);
   });
   
   irc.on('part' + details.channel, function(nick) {
-    sendMessage(nick + ' has left ' + details.channel);
+    if (nick.slice(-2) != '_s')
+      sendMessage(nick + ' has left ' + details.channel);
   });
   
   irc.on('quit', function(nick, reason) {
-    sendMessage(nick + ' has quit (' + reason + ')');
+    if (nick.slice(-2) != '_s')
+      sendMessage(nick + ' has quit (' + reason + ')');
+  });
+
+  irc.on('nick', function(oldnick, newnick, channels, message) { 
+    if (oldnick.slice(-2) != '_s')
+      steam.sendMessage(details.chatroom, oldnick + " is now known as " + newnick); 
   });
   
->>>>>>> 0293776f11ff21bce5011cd7f118e196ba676c3e
   var steam = new Steam.SteamClient();
   steam.logOn({
     accountName: details.username,
@@ -117,111 +151,36 @@ module.exports = function(details) {
     steam.setPersonaState(Steam.EPersonaState.Online);
     steam.joinChat(details.chatroom);
     
-<<<<<<< HEAD
-    irc.on('message' + details.channel, function(from, message) {
-      steam.sendMessage(details.chatroom, '<' + from + '> ' + message);
-      
-      var parts = message.match(/(\S+)\s+(.*\S)/);
-      
-      var triggers = {
-        '.k': 'kick',
-        '.kb': 'ban',
-        '.unban': 'unban'
-      };
-      
-      if (parts && parts[1] in triggers) {      
-        irc.whois(from, function(info) {
-          if (info.channels.indexOf('@' + details.channel) == -1)
-            return; // not OP, go away
-          
-          Object.keys(steam.users).filter(function(steamID) {
-            return steam.users[steamID].playerName == parts[2];
-          }).forEach(function(steamID) {
-            steam[triggers[parts[1]]](details.chatroom, steamID);
-          });
-        });
-      } else if (message.trim() == '!lu' || message.trim() == '~lu') {
-        response = "Current Users: " + Object.keys(steam.chatRooms[details.chatroom]).map(function(steamid){return steam.users[steamid].playerName;}).join(', ');
-        irc.say(details.channel, response)
-      }
-    });
-    
-    irc.on('action', function(from, to, message) {
-      if (to == details.channel) {
-        steam.sendMessage(details.chatroom, from + ' ' + message);
-      }
-    });
-    
-    irc.on('+mode', function(channel, by, mode, argument, message) {
-      if (channel == details.channel && mode == 'b') {
-        steam.sendMessage(details.chatroom, by + ' sets ban on ' + argument);
-      }
-    });
-    
-    irc.on('-mode', function(channel, by, mode, argument, message) {
-      if (channel == details.channel && mode == 'b') {
-        steam.sendMessage(details.chatroom, by + ' removes ban on ' + argument);
-      }
-    });
-    
-    irc.on('kick' + details.channel, function(nick, by, reason, message) {
-      steam.sendMessage(details.chatroom, by + ' has kicked ' + nick + ' from ' + details.channel + ' (' + reason + ')');
-    });
-    
-    irc.on('join' + details.channel, function(nick) {
-      steam.sendMessage(details.chatroom, nick + ' has joined ' + details.channel);
-    });
-    
-    irc.on('part' + details.channel, function(nick) {
-      steam.sendMessage(details.chatroom, nick + ' has left ' + details.channel);
-    });
-    
-    irc.on('quit', function(nick, reason) {
-      steam.sendMessage(details.chatroom, nick + ' has quit (' + reason + ')');
-    });
-
-    irc.on('nick', function(oldnick, newnick, channels, message) { 
-      steam.sendMessage(details.chatroom, oldnick + " is now known as " + newnick); 
-    });
-
-    irc.on('error', function(err) {
-      console.log('IRC error: ', err);
-    });
-=======
     queue.forEach(sendMessage);
     queue = [];
->>>>>>> 0293776f11ff21bce5011cd7f118e196ba676c3e
+    setTimeout(joinSteamBots, 100);
   });
   
   steam.on('chatMsg', function(chatRoom, message, msgType, chatter) {
-    var game = steam.users[chatter].gameName;
-    var name = steam.users[chatter].playerName;
-    if (msgType == Steam.EChatEntryType.ChatMsg) {
-      irc.say(details.channel, require('util').format(game ? msgFormatGame : msgFormat, name, message));
-    } else if (msgType == Steam.EChatEntryType.Emote) {
-      irc.say(details.channel, require('util').format(game ? emoteFormatGame : emoteFormat, name, message));
-    }
-    
     var parts = message.split(/\s+/);
     var permissions = steam.chatRooms[chatRoom][chatter].permissions;
     
-    if (parts[0] == '.k' && permissions & Steam.EChatPermission.Kick) {
-      irc.send('KICK', details.channel, parts[1], 'requested by ' + name);
-      
-    } else if (parts[0] == '.kb' && permissions & Steam.EChatPermission.Ban) {
-      irc.send('MODE', details.channel, '+b', parts[1]);
-      irc.send('KICK', details.channel, parts[1], 'requested by ' + name);
-      
-    } else if (parts[0] == '.unban' && permissions & Steam.EChatPermission.Ban) {
-      irc.send('MODE', details.channel, '-b', parts[1]);
-      
-    } else if (parts[0] == '!lu' || parts[0] == '~lu') {
+    if (parts[0] == '!lu' || parts[0] == '~lu') {
       irc.send('NAMES', details.channel);
       irc.once('names' + details.channel, function(nicks) {
         steam.sendMessage(details.chatroom, 'Users in ' + details.channel + ': ' + Object.keys(nicks).map(function(key) {
           return nicks[key] + key;
         }).join(', '));
       });
+    } else if (parts[0] == '!topic' || parts[0] == '~topic') {
+      var channel = irc.chanData(details.channel);
+      if ( channel ) {
+        sendMessage(channel.topic)
+      }
+    } else if (parts[0] == '/me') {
+      if (Object.keys(steamIRCBots).indexOf(chatter) != -1) {
+        steamIRCBots[chatter].action(details.channel, message.slice(4));
+      }
+      else {
+        irc.say(details.channel, steam.users[chatter].playerName + ' ' + message.slice(4));
+      }
+    } else {
+      sendIRCMessage(chatter, message)
     }
   });
   
@@ -229,47 +188,55 @@ module.exports = function(details) {
     var name = steam.users[chatterActedOn].playerName;
     switch (stateChange) {
       case Steam.EChatMemberStateChange.Entered:
-        irc.say(details.channel, name + ' entered chat.');
+        joinUser(chatterActedOn);
         break;
       case Steam.EChatMemberStateChange.Left:
-        irc.say(details.channel, name + ' left chat.');
+        if (Object.keys(steamIRCBots).indexOf(chatterActedOn) != -1) {
+          steamIRCBots[chatterActedOn].disconnect("Left chat.");
+          delete steamIRCBots[chatterActedOn];
+        }
         break;
       case Steam.EChatMemberStateChange.Disconnected:
-        irc.say(details.channel, name + ' disconnected.');
+        if (Object.keys(steamIRCBots).indexOf(chatterActedOn) != -1) {
+          steamIRCBots[chatterActedOn].disconnect("Disconnected from chat.");
+          delete steamIRCBots[chatterActedOn];
+        }
         break;
       case Steam.EChatMemberStateChange.Kicked:
-        irc.say(details.channel, name + ' was kicked by ' + steam.users[chatterActedBy].playerName + '.');
+        if (Object.keys(steamIRCBots).indexOf(chatterActedOn) != -1) {
+          steamIRCBots[chatterActedOn].disconnect('Kicked by ' + steam.users[chatterActedBy].playerName + '.');
+          delete steamIRCBots[chatterActedOn];
+        }
         break;
       case Steam.EChatMemberStateChange.Banned:
-        irc.say(details.channel, name + ' was banned by ' + steam.users[chatterActedBy].playerName + '.');
+        if (Object.keys(steamIRCBots).indexOf(chatterActedOn) != -1) {
+          steamIRCBots[chatterActedOn].disconnect('Banned by ' + steam.users[chatterActedBy].playerName + '.');
+          delete steamIRCBots[chatterActedOn];
+        }
     }
   });
-  
+
   steam.on('user', function(user) {
     if (user.friendid in steam.users && steam.chatRooms[details.chatroom] != undefined)
       if (user.friendid in steam.chatRooms[details.chatroom])
         if (steam.users[user.friendid].playerName != user.playerName && steam.users[user.friendid].playerName != '' && user.playerName != '')
-          irc.say(details.channel, steam.users[user.friendid].playerName + ' is now known as ' + user.playerName);
+        {
+          steamIRCBots[user.friendid].send("NICK", createValidNick(user.playerName) + "_s");
+        }
   });
   
   steam.on('loggedOff', function(result) {
     console.log("Logged off:", result);
-    console.log("Removing Event Listeners");
-    irc.removeAllListeners('message' + details.channel);
-    irc.removeAllListeners('action');
-    irc.removeAllListeners('+mode');
-    irc.removeAllListeners('-mode');
-    irc.removeAllListeners('kick' + details.channel);
-    irc.removeAllListeners('join' + details.channel);
-    irc.removeAllListeners('part' + details.channel);
-    irc.removeAllListeners('quit');
-    irc.removeAllListeners('nick');
-    irc.removeAllListeners('error');
+    for (user in steamIRCBots) {
+      steamIRCBots[user].disconnect("Steam seems down");
+      delete steamIRCBots[user];
+    }
+    botsJoined = false;
   });
   
   steam.on('sentry', function(data) {
-    require('fs').writeFileSync('sentry', data)
-;  })
+    require('fs').writeFileSync('sentry', data);
+  })
   
   steam.on('debug', console.log);
 };
